85a95bcd5a5fddf55ccf90012ffb907787431312
Fabien Davy <davyfabien@gmail.com>
2020-05-28 21:17:41 +0200

    resuming work

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,14 @@
 class PagesController < ApplicationController
   def dashboard
     if params[:country]
-      
+      selected_country = params[:country] if params[:country] != 'All'
     end
 
     @countries = get_countries
-    @orders = get_orders_of_a_country
-    raise
-
-    @number_of_customers = get_numbers_of_customers
+    @revenue = get_revenue(selected_country)
+    @average_revenue = (@revenue.fdiv(get_orders_number(selected_country))).round(2)
+    @number_of_customers = get_customers_number(selected_country)
+    # raise
   end
 
   private
@@ -17,14 +17,30 @@ class PagesController < ApplicationController
     Customer.all.each_with_object([]) { |customer, arr| arr << customer.country unless arr.include?(customer.country) }
   end
 
-  def get_numbers_of_customers
-    params[:country] != 'All' ? Customer.where(country: params[:country]).count : Customer.count
+  def get_customers_number(country)
+    country ? Customer.where(country: country).count : Customer.count
   end
 
-  def get_revenue
-    Customer.where(country: params[:country]).each do |customer|
-      customer.orders.
+  def get_orders_number(country)
+    country ? Order.where(customer: Customer.where(country: country)).count : Order.count
+  end
+
+  def get_revenue(country)
+    total_revenue = 0
+    if country
+      Customer.where(country: country).each do |customer|
+        customer.orders.each do |order|
+          total_revenue += order.total_price
+        end
+      end
+    else
+      # Order.all.each { |order| total_revenue += order.total_price }
+      OrderProduct.all.each do |order_product|
+        product = Product.find(order_product.product_id)
+        total_revenue += product.unit_price * order_product.quantity
+      end
     end
+    total_revenue.round(1)
   end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -7,6 +7,11 @@ class Order < ApplicationRecord
   validates :customer, presence: true
 
   def total_price
-    
+    order_sum = 0
+    order_products.each do |order_product|
+      product = Product.find(order_product.product_id)
+      order_sum += product.unit_price * order_product.quantity
+    end
+    order_sum.round(2)
   end
 end
